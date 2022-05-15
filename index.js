319cddd46f8f7859649a5f36e9ddd90d06c90d35
amelmennad <amelmnd.dev@gmail.com>
2022-05-15 20:24:00 +0200

    Fix : uninstall cors

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const dotenv = require("dotenv");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 const uid2 = require("uid2");
-
 const User = require("./models/User");
 
 const app = express();
